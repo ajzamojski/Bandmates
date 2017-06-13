278dae3420103d797db033ded5940a4885cde468
Adam Zamojski <adamzamojski@Adams-MBP.home>
2017-06-13 18:23:22 -0400

    added initial files

---
+++ src/components/Main.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+
+	render () {
+
+		return (
+
+			<div className ="container">
+				<div className="jumbotron">
+					<h1>Welcome to Bandmates</h1>
+				</div>
+			</div>
+
+			)
+	}
+}
+
+export default Main;
