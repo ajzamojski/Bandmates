f333c0666cfa9f8523c23d4c3b18bee1511e4d60
Ralph Manlapig <ralphmanlap@gmail.com>
2017-06-19 21:31:08 -0400

    changed team members on homepage, font on sidebar

--- src/components/Home.js
+++ src/components/Home.js
@@ -184,7 +184,7 @@ var Home = React.createClass({
         {/*Team Section*/}
         <section id="team" className="bg-light-gray">
             <div className="container">
-                <div className="row">
+                 <div className="row">
                     <div className="col-lg-12 text-center">
                         <h2 className="section-heading">Our Amazing Team</h2>
                         <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
@@ -193,9 +193,14 @@ var Home = React.createClass({
                 <div className="row">
                     <div className="col-sm-4">
                         <div className="team-member">
-                            <img src="#" className="img-responsive img-circle" alt=""/>
-                            <h4>Kevin Lee</h4>
-                            <p className="text-muted">Web Developer</p>
+                           <figure className="snip1543">
+                              <img src="https://avatars0.githubusercontent.com/u/19822314?v=3&s=400" alt="sample108" />
+                              <figcaption>
+                                <h3>Kevin Lee</h3>
+                                <p>The only skills I have the patience to learn are those that have no real application in life.</p>
+                              </figcaption>
+                              <a href="#"></a>
+                            </figure>
                             <ul className="list-inline social-buttons">
                                 <li><a href="#"><i className="fa fa-twitter"></i></a>
                                 </li>
@@ -208,9 +213,13 @@ var Home = React.createClass({
                     </div>
                     <div className="col-sm-4">
                         <div className="team-member">
-                            <img src="#" className="img-responsive img-circle" alt=""/>
-                            <h4>Ralph Manlapig</h4>
-                            <p className="text-muted">Web Developer</p>
+                            <figure className="snip1543"><img src="https://avatars3.githubusercontent.com/u/23372535?v=3&s=400" alt="sample100" />
+                              <figcaption>
+                                <h3>Adam Zamojski</h3>
+                                <p>The real fun of living wisely is that you get to be smug about it.</p>
+                              </figcaption>
+                              <a href="#"></a>
+                            </figure>
                             <ul className="list-inline social-buttons">
                                 <li><a href="#"><i className="fa fa-twitter"></i></a>
                                 </li>
@@ -223,9 +232,13 @@ var Home = React.createClass({
                     </div>
                     <div className="col-sm-4">
                         <div className="team-member">
-                            <img src="#" className="img-responsive img-circle" alt=""/>
-                            <h4>Adam Zamojski</h4>
-                            <p className="text-muted">Web Developer</p>
+                            <figure className="snip1543"><img src="http://i.imgur.com/V56f3Uv.jpg" alt="sample101" />
+                              <figcaption>
+                                <h3>Ralph Manlapig</h3>
+                                <p>But Calvin is no kind and loving god! He's one of the old gods! He demands sacrifice! </p>
+                              </figcaption>
+                              <a href="#"></a>
+                            </figure>
                             <ul className="list-inline social-buttons">
                                 <li><a href="#"><i className="fa fa-twitter"></i></a>
                                 </li>
