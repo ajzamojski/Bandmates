f25266e2d538967d967044284e1ff70c8a605499
Kevin Lee <lee.kev1991@gmail.com>
2017-06-26 19:39:28 -0400

    latest commit

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -19,10 +19,29 @@ var Profile = React.createClass ({
   	},
 	render: function () {
 		return (
-			<div className ="container">
-				<div className="row">
-					<div className="jumbotron">
-						<h1>Profile</h1>
+			<div >
+				<div className="container">
+
+					<div className="container">
+
+						<div className="container" id="banner" style={{display: 'block', height: '15em'}}>
+							<h1 className="text-center">{this.state.firstName + " " + this.state.lastName + "'s Profile"}</h1>
+						</div>
+
+						<div className="row" style={{ height:'40em', border: 'solid 1px black'}}>
+							<div className="container col-xs-4" id="" style={{display: 'block', height:'100%', border: 'solid 1px black'}}>
+								<h3 className="text-center">Left Side</h3>
+							</div>
+						
+							<div className="container col-xs-8" id="" style={{display: 'block', height:'100%', border: 'solid 1px black'}}>
+								<h3 className="text-center">Right Side</h3>
+							</div>
+						</div>
+						<div className="row" >
+							<div className="container col-xs-12" id="" style={{height:'25em', border: 'solid 1px black'}}>
+								<h3 className="text-center">Bottom</h3>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
