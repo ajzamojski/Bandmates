dc04aad7f61aa1c393fe8e66f22d451d045c9c84
Ralph Manlapig <ralphmanlap@gmail.com>
2017-07-05 17:13:21 -0400

    removing some commented out code

--- config/passport.js
+++ config/passport.js
@@ -107,10 +107,7 @@ passport.use('local-login', new LocalStrategy({
 	    	return bCrypt.compareSync(password, testUser);
 
 			};
-			// var passedPassword = generateHash(req.body.password);
-   //  	var dbPassword = logUser.password.toString();
-    	//  console.log(passedPassword);
-    	// console.log(dbPassword);
+
 
     	if (!logUser) {
     		console.log("user was not found");
@@ -127,61 +124,3 @@ passport.use('local-login', new LocalStrategy({
 	});
 }));
 
-// passport.use('local-update', new LocalStrategy({
-//     usernameField: 'email',
-//     passwordField: 'password',
-//     passReqToCallback: true
-// }, function(req, email, password, done) {
-//         console.log("passport is hit");
-         
-//       var generateHash = function(password) {
-
-//         return bCrypt.hashSync(password, bCrypt.genSaltSync(8), null);
-
-//         };
-
-//         db.User.findOne({
-//         where: {
-//            email: email
-//         }
-//     }).then(function(signUser) {
-//         console.log("does the user exist? ");
-//         console.log(signUser);
-
-//         if (signUser) {
-//             console.log("user already exists");
-//             return done(null, false, req.flash('signupMessage', 'That email is already taken'));
-//         }
-
-//         else {
-
-//             var firstName = req.body.firstName;
-//             var lastName = req.body.lastName;
-//             var username = req.body.userName;
-//             var email = req.body.email;
-//             // var password = User.generateHash(req.body.password);
-//             // var password2 = User.generateHash(req.body.password2);
-//             var password = generateHash(req.body.password);
-//             var password2 = req.body.password2;
-//             console.log(req.body);
-
-//             // var newUserMysql = new Object();
-//             // newUserMysql.email    = email;
-//             // newUserMysql.password = password;
-//             // console.log(newUserMysql);
-
-//             db.User.create({firstName: firstName, lastName: lastName, username: username, email: email, password: password})
-//                 .done(function(data) {
-//                     // if (err) {
-//                     //  return done(err);
-//                     // }
-//                     // newUserMysql.id = data.insertId;
-
-//                     console.log("the final data");
-//                     console.log(data);
-//                     return done(null, data, req.flash('signupMessage', 'You are now registered'));
-
-//                 });
-//             }
-//     });
-// }));
